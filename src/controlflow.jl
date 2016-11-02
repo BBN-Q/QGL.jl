78c96b404b628b1f5d8f8031b5196215bab03a39
Colm Ryan <colm@colmryan.org>
2016-11-02 17:30:29 -0400

    fill out more controlflow operations

--- src/controlflow.jl
+++ src/controlflow.jl
@@ -1,7 +1,20 @@
+import Base.show
+
+@enum CONTROL_OP WAIT SYNC LOAD_REPEAT REPEAT GOTO CALL RETURN
+
 immutable ControlFlow
 	label::AbstractString
+	op::CONTROL_OP
+	target
+	value
 end
 
 export WAIT
 
-WAIT() = ControlFlow("WAIT")
+wait() = ControlFlow("INIT", WAIT, 0, 0)
+
+function show(io::IO, cf::ControlFlow)
+	if cf.op == WAIT
+		print(io, cf.label)
+	end
+end
