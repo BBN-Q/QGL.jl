7fa54db9aaabde2a8e8cdcf22a35be53225423d4
Colm Ryan <colm@colmryan.org>
2017-01-18 21:42:46 -0500

    backup original then create new cfg file pointing to test ChannelParams.json

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,30 @@
-io = IOBuffer(joinpath(@__FILE__, "ChannelParams.json")*"\n")
-redirect_stdin(io)
+# setup cfg to point to test channel parameters JSON file
+import JSON
+cfg_folder = joinpath(dirname(@__FILE__), "..", "cfg")
+cfg_file = joinpath(cfg_folder, "cfg.json")
+# backup existing one
+restore_backup = false
+if isfile(cfg_file)
+	cp(cfg_file, cfg_file*".orig")
+	restore_backup = true
+end
+open(cfg_file, "w") do f
+	JSON.print(f, Dict{String,String}("channel_params_file" => joinpath(dirname(@__FILE__), "ChannelParams.json")))
+end
+
 using QGL
-redirect_stdin(STDIN)
 
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+try
+@testset "Pulse Creation" begin
+p = X90(q1)
+@test p.label = "X90"
+end
+
+finally
+if restore_backup
+	mv(cfg_file*".orig", cfg_file, remove_destination=true)
+end
+
+end
