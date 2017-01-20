625be1ca460bef5303e26c9037dea5ef45cab444
Colm Ryan <colm@colmryan.org>
2017-01-19 20:15:05 -0500

    make cfg folder if necessary in testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,12 +8,14 @@ if isfile(cfg_file)
 	cp(cfg_file, cfg_file*".orig")
 	restore_backup = true
 end
+if !isdir(cfg_folder)
+	mkdir(cfg_folder)
+end
 open(cfg_file, "w") do f
 	JSON.print(f, Dict{String,String}("channel_params_file" => joinpath(dirname(@__FILE__), "ChannelParams.json")))
 end
 
 using QGL
-
 using Base.Test
 
 try
