208c37e7f2a18be5b6686a4224e24c39905f1b17
Diego Ristè <diego.riste@raytheon.com>
2018-04-18 11:01:53 -0400

    Basic config for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,27 +1,28 @@
 # setup cfg to point to test channel parameters JSON file
-import JSON
+import YAML
 cfg_folder = joinpath(dirname(@__FILE__), "..", "cfg")
-cfg_file = joinpath(cfg_folder, "cfg.json")
+cfg_file = joinpath(cfg_folder, "test_measure.yml")
 # backup existing one
-restore_backup = false
-if isfile(cfg_file)
-	cp(cfg_file, cfg_file*".orig")
-	restore_backup = true
-end
-if !isdir(cfg_folder)
-	mkdir(cfg_folder)
-end
-sequence_files_path = joinpath(dirname(@__FILE__), "sequence_files")
-if !isdir(sequence_files_path)
-	mkdir(sequence_files_path)
-end
-open(cfg_file, "w") do f
-	cfg = Dict{String,String}()
-	cfg["channel_params_file"] = joinpath(dirname(@__FILE__), "ChannelParams.json")
-	cfg["instrument_params_file"] = joinpath(dirname(@__FILE__), "Instruments.json")
-	cfg["sequence_files_path"] = sequence_files_path
-	JSON.print(f, cfg)
-end
+# restore_backup = false
+# if isfile(cfg_file)
+# 	cp(cfg_file, cfg_file*".orig")
+# 	restore_backup = true
+# end
+# if !isdir(cfg_folder)
+# 	mkdir(cfg_folder)
+# end
+
+# sequence_files_path = joinpath(dirname(@__FILE__), "sequence_files")
+# if !isdir(sequence_files_path)
+# 	mkdir(sequence_files_path)
+# end
+# open(cfg_file, "w") do f
+# 	cfg = Dict{String,String}()
+# 	cfg["channel_params_file"] = joinpath(dirname(@__FILE__), "ChannelParams.json")
+# 	cfg["instrument_params_file"] = joinpath(dirname(@__FILE__), "Instruments.json")
+# 	cfg["sequence_files_path"] = sequence_files_path
+# 	JSON.print(f, cfg)
+# end
 
 using QGL
 using Base.Test
