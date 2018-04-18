988698abb573d56e9afaea1aa08e3bdfda39de92
Diego Ristè <diego.riste@raytheon.com>
2018-04-18 17:25:44 -0400

    Try simple fix for test config

--- src/config.jl
+++ src/config.jl
@@ -7,50 +7,30 @@ module config
 import YAML
 
 cfg_folder = joinpath(Pkg.dir("QGL"), "cfg")
-cfg_file = joinpath(cfg_folder, "measure.yml")
+cfg_path = joinpath(cfg_folder, "cfg_path.txt")
 # for simplicity here use single config file
 
-#if isdir(cfg_folder) && isfile(cfg_file)
-cfg = YAML.load_file(cfg_file)
-#instrument_json_file = get(cfg, "instrument_params_file", "")
-sequence_files_path = get(cfg, "AWGDir", "")
+if isdir(cfg_folder) && isfile(cfg_path)
+    f = open(cfg_path)
+    cfg_yaml = readstring(f)
+    close(f)
 
-#TODO: prompts for config path
-#else
-	# println("Please provide path to channel parameters JSON file:")
-	# channel_json_file = chomp(readline())
-	# println("Please provide path to instrument parameters JSON file:")
-	# instrument_json_file = chomp(readline())
-	# println("Please provide path to where sequence files should be output to:")
-	# sequence_files_path = chomp(readline())
-	#
-	# if !isdir(cfg_folder)
-	# 	mkdir(cfg_folder)
-	# end
-	# open(cfg_file, "w") do f
-	# 	JSON.print(f,
-	# 		Dict{String,String}(
-	# 			"channel_params_file" => channel_json_file,
-	# 			"instrument_params_file" => instrument_json_file,
-	# 			"sequence_files_path" => sequence_files_path
-	# 		)
-	# 	)
-	# end
-#end
+else
+	println("Please provide path to yaml settings file:")
+	 cfg_yaml = chomp(readline())
 
+	if !isdir(cfg_folder)
+		mkdir(cfg_folder)
+	end
+	open(cfg_path, "w") do f
+        write(f, cfg_yaml)
+    end
+end
+
+cfg = YAML.load_file(cfg_yaml)
 get_qubit_params() = cfg["qubits"]
 get_marker_params() = cfg["markers"]
 get_edge_params() = cfg["edges"]
 get_instrument_params() = cfg["instruments"]
-
-# TODO: is this useful or perhaps all we can do with immutable channel objects
-# watch the channel JSON file
-# @async begin
-# 	while true
-# 		event = watch_file("/home/cryan/Programming/Repos/PyQLab/cfg/ChannelParams.json")
-# 		if event.changed
-# 			warn("ChannelParams file updated.")
-# 	end
-# end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,25 @@
 # setup cfg to point to test channel parameters JSON file
 import YAML
-# cfg_folder = joinpath(dirname(@__FILE__), "..", "cfg")
-# cfg_file = joinpath(cfg_folder, "measure.yml")
+cfg_folder = joinpath(dirname(@__FILE__), "..", "cfg")
+cfg_file = joinpath(cfg_folder, "cfg_path.txt")
+
 # backup existing one
-# restore_backup = false
-# if isfile(cfg_file)
-# 	cp(cfg_file, cfg_file*".orig")
-# 	restore_backup = true
-# end
-# if !isdir(cfg_folder)
-# 	mkdir(cfg_folder)
-# end
+restore_backup = false
+if isfile(cfg_file)
+	cp(cfg_file, cfg_file*".orig")
+	restore_backup = true
+end
+if !isdir(cfg_folder)
+	mkdir(cfg_folder)
+end
 
 # sequence_files_path = joinpath(dirname(@__FILE__), "sequence_files")
-# if !isdir(sequence_files_path)
-# 	mkdir(sequence_files_path)
-# end
-# open(cfg_file, "w") do f
+if !isdir(sequence_files_path)
+	mkdir(sequence_files_path)
+end
+open(cfg_file, "w") do f
+	write(f, "test_measure.yml")
+end
 # 	cfg = Dict{String,String}()
 # 	cfg["channel_params_file"] = joinpath(dirname(@__FILE__), "ChannelParams.json")
 # 	cfg["instrument_params_file"] = joinpath(dirname(@__FILE__), "Instruments.json")
@@ -39,9 +42,9 @@ p = X90(q1)
 @test p.label == "X90"
 end
 
-# finally
-# if restore_backup
-# 	mv(cfg_file*".orig", cfg_file, remove_destination=true)
-# end
+finally
+if restore_backup
+	mv(cfg_file*".orig", cfg_file, remove_destination=true)
+end
 
 end
